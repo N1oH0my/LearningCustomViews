55d61eda450fe1bee0199293bd8d0c4bcec24ac4
N1oH0my <h0myn1o@mail.ru>
2024-09-27 16:31:36 +0300

    SI-my - Добавлена кастомная Drawable - LandscapeDrawable

---
+++ app/src/main/java/com/surf2024/learningcustomviews/features/landscape/LandscapeItem.kt
@@ -0,0 +1,29 @@
+package com.surf2024.learningcustomviews.features.landscape
+
+import android.graphics.Canvas
+import android.graphics.Paint
+
+
+abstract class LandscapeItem(val paint: Paint) {
+    var x: Float = 0f
+    var y: Float = 0f
+    var width: Float = 0f
+    var height: Float = 0f
+
+    fun setSize(width: Float, height: Float) {
+        this.width = width
+        this.height = height
+        onSizeChanged()
+    }
+
+    open fun onSizeChanged() {}
+
+    fun draw(canvas: Canvas) {
+        canvas.save()
+        canvas.translate(x, y)
+        onDraw(canvas)
+        canvas.restore()
+    }
+
+    abstract fun onDraw(canvas: Canvas)
+}
