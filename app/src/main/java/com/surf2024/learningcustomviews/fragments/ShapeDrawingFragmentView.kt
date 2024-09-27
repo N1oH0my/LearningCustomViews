a032f6a78f033b34879ce5323a822da60405c2d4
N1oH0my <h0myn1o@mail.ru>
2024-09-27 16:06:26 +0300

    SI-10475 - ShapeDrawingView добавлена во фрагмент, задан набор цветов для фигур и цвет по-умолчанию

--- app/src/main/java/com/surf2024/learningcustomviews/fragments/ShapeDrawingFragmentView.kt
+++ app/src/main/java/com/surf2024/learningcustomviews/fragments/ShapeDrawingFragmentView.kt
@@ -1,11 +1,13 @@
 package com.surf2024.learningcustomviews.fragments
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.surf2024.learningcustomviews.R
+import com.surf2024.learningcustomviews.features.ShapeDrawingView
 
 class ShapeDrawingFragmentView : Fragment() {
 
@@ -13,7 +15,10 @@ class ShapeDrawingFragmentView : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_shape_drawing_view, container, false)
+        val view = inflater.inflate(R.layout.fragment_shape_drawing_view, container, false)
+        val shapeDrawingView = view.findViewById<ShapeDrawingView>(R.id.shapeDrawingView)
+        shapeDrawingView.setColors(listOf(Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW))
+        return view
     }
 
 }
