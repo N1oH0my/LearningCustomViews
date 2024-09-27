58ef5b4d2127c192c09b5224b018e16b2e8cf601
N1oH0my <h0myn1o@mail.ru>
2024-09-27 17:42:13 +0300

    SI-my_avatars - Добавлены kotlin-docs

--- app/src/main/java/com/surf2024/learningcustomviews/features/AvatarsLineView.kt
+++ app/src/main/java/com/surf2024/learningcustomviews/features/AvatarsLineView.kt
@@ -12,8 +12,20 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CircleCrop
 import com.bumptech.glide.request.target.SimpleTarget
 import com.bumptech.glide.request.transition.Transition
+import com.surf2024.learningcustomviews.features.AvatarsLineView.Companion.MAX_IMAGES
 
-class AvatarsLineView@JvmOverloads constructor(
+/**
+ * `View` для отображения круглыхх изображений в линию.
+ *
+ * Этот класс рисует до [MAX_IMAGES] круглых изображений, загруженных по `URL`,
+ * и, если изображений больше [MAX_IMAGES], накладывает индикатор на дополнительный круг,
+ * указывающий количество дополнительных изображений.
+ *
+ * @property [context] Контекст, связанный с этим видом.
+ * @property [attrs] Необязательный набор атрибутов для пользовательских XML атрибутов.
+ * @property [defStyleAttr] Атрибут стиля по умолчанию.
+ */
+class AvatarsLineView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
@@ -22,6 +34,15 @@ class AvatarsLineView@JvmOverloads constructor(
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
     private var imageBitmaps: List<Bitmap?> = emptyList()
 
+    /**
+     * Устанавливает список `URL` изображений, которые будут отображаться как аватары.
+     *
+     * Этот метод загружает изображения с помощью `Glide`,
+     * применяет круглую обрезку
+     * и обновляет `view` с загруженными битмапами.
+     *
+     * @param [urls] Список URL изображений для загрузки и отображения.
+     */
     fun setImages(urls: List<String>) {
         imageBitmaps = mutableListOf()
         for (url in urls) {
@@ -30,7 +51,10 @@ class AvatarsLineView@JvmOverloads constructor(
                 .load(url)
                 .transform(CircleCrop())
                 .into(object : SimpleTarget<Bitmap>() {
-                    override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                    override fun onResourceReady(
+                        resource: Bitmap,
+                        transition: Transition<in Bitmap>?
+                    ) {
                         (imageBitmaps as MutableList).add(resource)
                         invalidate()
                     }
@@ -38,6 +62,13 @@ class AvatarsLineView@JvmOverloads constructor(
         }
     }
 
+    /**
+     * Рисует `view`. Этот метод вызывается каждый раз, когда `view` необходимо перерисовать.
+     *
+     * Он вычисляет позиции для каждого аватара и вызывает соответствующие методы рисования.
+     *
+     * @param [canvas] Холст.
+     */
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
@@ -45,16 +76,31 @@ class AvatarsLineView@JvmOverloads constructor(
         val centerX = imageRadius / 2 + imagesMargin
         val centerY = imageRadius / 2 + imagesMargin
 
-        for (i in 0 until minOf(count, 4)) {
+        for (i in 0 until minOf(count, MAX_IMAGES)) {
             drawCircleImage(canvas, imageBitmaps[i], centerX, centerY, i)
         }
 
-        if (count > 4) {
-            drawOverlayCircle(canvas, centerX, centerY, count - 4)
+        if (count > MAX_IMAGES) {
+            drawOverlayCircle(canvas, centerX, centerY, count - MAX_IMAGES)
         }
     }
 
-    private fun drawCircleImage(canvas: Canvas, bitmap: Bitmap?, centerX: Float, centerY: Float, index: Int) {
+    /**
+     * Рисует круглое изображение в заданной позиции на холсте.
+     *
+     * @param [canvas] Холст, на котором будет нарисовано изображение.
+     * @param [bitmap] Битмап для рисования. Если `null`, ничего не рисуется.
+     * @param [centerX] X-координата центра изображения.
+     * @param [centerY] Y-координата центра изображения.
+     * @param [index] Индекс изображения в списке, используемый для позиционирования.
+     */
+    private fun drawCircleImage(
+        canvas: Canvas,
+        bitmap: Bitmap?,
+        centerX: Float,
+        centerY: Float,
+        index: Int
+    ) {
         if (bitmap != null) {
             val left = centerX - imageRadius + index * imagesMargin
             val top = centerY - imageRadius
@@ -65,7 +111,22 @@ class AvatarsLineView@JvmOverloads constructor(
         }
     }
 
-    private fun drawOverlayCircle(canvas: Canvas, centerX: Float, centerY: Float, remaining: Int, index: Int = 4) {
+    /**
+     * Рисует накладной круг, указывающий количество оставшихся изображений.
+     *
+     * @param [canvas] Холст, на котором будет нарисована накладка.
+     * @param [centerX] X-координата центра накладки.
+     * @param [centerY] Y-координата центра накладки.
+     * @param [remaining] Количество оставшихся изображений для отображения.
+     * @param [index] Индекс позиции накладки (по умолчанию [MAX_IMAGES]).
+     */
+    private fun drawOverlayCircle(
+        canvas: Canvas,
+        centerX: Float,
+        centerY: Float,
+        remaining: Int,
+        index: Int = MAX_IMAGES
+    ) {
         paint.color = ContextCompat.getColor(context, android.R.color.darker_gray)
         val left = centerX - imageRadius + index * imagesMargin
         val top = centerY - imageRadius
@@ -85,8 +146,13 @@ class AvatarsLineView@JvmOverloads constructor(
     }
 
     companion object {
+        /** Максимальное количество изобрежаний. */
+        const val MAX_IMAGES = 4
+        /** Радиус по умолчанию для изображений. */
         const val imageRadius = 200f
+        /** Отступ между изображениями. */
         const val imagesMargin = 150f
+        /** Размер текста для индикатора накладки. */
         const val textSize = 100f
     }
 
